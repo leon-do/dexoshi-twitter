f04d174ca810820734b82fa4eb4f48d46d67cfb8
leon-do <leondo123@gmail.com>
2023-01-02 14:25:28 -0500

    rm lastMint

--- src/handleMint.js
+++ src/handleMint.js
@@ -6,17 +6,6 @@ const displayCard = require("./displayCard");
  * Mint anyone who retweets this tweet
  */
 module.exports = async function handleMint(_twitter, _tweet) {
-  // call "twitterIdToAddress" to get address on chain
-  const toAddress = await contract["twitterIdToAddress"](_tweet.data.author_id);
-  // get lastMint
-  const lastMint = await contract["lastMint"](toAddress);
-  // if last mint is too soon, then return error
-  const secondsToMint = 300;
-  if (Math.floor(Date.now() / 1000) + secondsToMint < lastMint) {
-    const secondsLeft = Math.floor(lastMint - Math.floor(Date.now() / 1000) + secondsToMint);
-    const message = `@${_tweet.data.author_id} must wait ${secondsLeft} seconds between mints`;
-    return await _twitter.v2.reply(message, _tweet.data.id);
-  }
   // get twitter handle from author_id
   const toHandle = await _twitter.v2.users([_tweet.data.author_id]);
   // get tokenId from command: "RT @dexoshi: @dexoshi mint 8"
