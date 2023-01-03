fc238b9c20ef85e06ca61b52ce00755c8914ecee
leon-do <leondo123@gmail.com>
2023-01-03 00:43:19 -0500

    contract metadata

--- src/tweetMint.js
+++ src/tweetMint.js
@@ -5,6 +5,6 @@ const metadata = require("./metadata.json");
 module.exports = async function tweetMint() {
   const tokenId = 55;
   const description = metadata[tokenId].description;
-  const message = `@dexoshi mint ${tokenId} available \nName: ${description} \nCard ID: ${tokenId}. \n\nRetweet to claim`;
+  const message = `@dexoshi mint ${tokenId} available \nName: ${description} \nCard ID: ${tokenId}. \n\nRetweet to mint`;
   tweetWithCard(twitter, message, tokenId);
 };
