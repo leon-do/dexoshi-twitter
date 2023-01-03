5a7ce1bbec4f6317c638f021ae0dc5ab9d0bd016
leon-do <leondo123@gmail.com>
2023-01-02 20:09:57 -0500

    export tweetMint

--- src/tweetMint.js
+++ src/tweetMint.js
@@ -1,13 +1,10 @@
-require("dotenv").config();
 const twitter = require("./twitter");
 const tweetWithCard = require("./tweetWithCard");
 const metadata = require("./metadata.json");
 
-async function tweetMint() {
+module.exports = async function tweetMint() {
   const tokenId = 55;
-  const description = metadata[tokenId].description
+  const description = metadata[tokenId].description;
   const message = `@dexoshi mint ${tokenId} available \nName: ${description} \nCard ID: ${tokenId}. \n\nRetweet to claim`;
   tweetWithCard(twitter, message, tokenId);
-}
-
-tweetMint();
+};
