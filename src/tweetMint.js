91d7102c3caf9f5cc5b77e5954563c9f077f27bd
leon-do <leondo123@gmail.com>
2023-01-04 01:16:45 -0500

    multiple 20

--- src/tweetMint.js
+++ src/tweetMint.js
@@ -3,8 +3,8 @@ const tweetWithCard = require("./tweetWithCard");
 const metadata = require("./metadata.json");
 
 module.exports = async function tweetMint() {
-  // pick random tokenId from 0 to 20000. make sure it's divisible by 10
-  const tokenId = Math.floor((Math.random() * 20000) / 10) * 10;
+  // pick random tokenId from 0 to 20000. make sure it's divisible by 20
+  const tokenId = Math.floor((Math.random() * 20000) / 20) * 20;
   const description = metadata[tokenId].description;
   const message = `@dexoshi mint ${tokenId} available \nName: ${description} \nCard ID: #${tokenId} \n\nRetweet to mint`;
   tweetWithCard(twitter, message, tokenId);
