eb960acba935894c77b69c6698348b4d3e86acf4
leon-do <leondo123@gmail.com>
2023-01-07 12:16:08 -0500

    rm @

--- src/handleTransfer.js
+++ src/handleTransfer.js
@@ -20,7 +20,7 @@ module.exports = async function handleTransfer(_twitter, _tweet) {
     // call "adminSafeTransferFrom"
     const { hash } = await contract["adminSafeTransferFrom"](fromAddress, toAddress, tokenId, amount);
     // reply with tx hash
-    const message = `@${fromHandle.data[0].username} transfered Card ID: #${tokenId} to ${toAddress} \n${process.env.BLOCK_EXPLORER}/tx/${hash}`;
+    const message = `${fromHandle.data[0].username} transfered Card ID: #${tokenId} to ${toAddress} \n${process.env.BLOCK_EXPLORER}/tx/${hash}`;
     await _twitter.v2.reply(message, _tweet.data.id);
   } catch (err) {
     console.error("handleTrasnfer error:", err);
